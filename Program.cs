ac20d592ce0964fc3d48650169b6beabf4911023
ZhouQY <ZQY104@163.COM>
2018-07-23 20:22:23 +0800

    ThinkPad
    0723

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VScode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod1();
+            NewMethod1();
+            New.NewMethod12();
+            
+            // cal test = new cal();
+            // // int t= test.cal();
+            // Console.WriteLine("{0}",j);
+
+        }
+
+        private static void NewMethod1()
+        {
+            Console.WriteLine("恬恬\n");
+        }
+
+        private static void NewMethod31()
+        {
+            Console.WriteLine("3333");
+        }
+    }
+}
---
+++ class/NewMain12.cs
@@ -0,0 +1,22 @@
+using System;
+class New
+{
+            public static void NewMethod12()
+        {
+            Console.WriteLine("3333444");
+        }
+
+}
+
+public class cal
+{
+ static int cal1(int i)
+ {
+      i =100;
+      int j;
+      j =2*i;
+      return j;
+
+
+ }
+}
