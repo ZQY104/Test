a390f7f6401a9f1797f1d785ad6426f0504d4032
Zhou <stone.zhou@emerson.com>
2018-07-24 10:06:18 +0800

    remove class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿
+using System;
+using System.Reflection;
 
 namespace VScode
 {
     
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main()
+        {/* 
+            GetString Number = new GetString();
+            Number.SourceStr = "-12.5 deg c";
+            Number.Pattern ="\\d+\\.?\\d*";*/
+            string i ="hello world";
+            Console.WriteLine("{0}",i);
 
         }
 
